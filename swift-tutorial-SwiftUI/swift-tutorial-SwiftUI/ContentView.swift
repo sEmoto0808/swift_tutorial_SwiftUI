ffe805ec9e2eb71e2d4e3b0734af947ed9e9b1fa
Sho Emoto <sho.emoto@gmail.com>
2020-11-23 18:45:04 +0900

    サンプルプロジェクト追加

---
+++ swift-tutorial-SwiftUI/swift-tutorial-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-tutorial-SwiftUI
+//
+//  Created by 江本匠 on 2020/11/23.
+//  Copyright © 2020 S.Emoto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
