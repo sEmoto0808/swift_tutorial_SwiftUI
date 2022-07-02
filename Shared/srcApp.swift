f78124f65f0a0cd6e49396591eff2cbd7f9bccda
Sho Emoto <dd2d00seg1@gmail.com>
2022-07-02 15:45:46 +0900

    renewal project

---
+++ Shared/srcApp.swift
@@ -0,0 +1,17 @@
+//
+//  srcApp.swift
+//  Shared
+//
+//  Created by Sho Emoto on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct srcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
