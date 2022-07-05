79aa3b7d875aad12f518cebc849f09fceba5d392
Sho Emoto <dd2d00seg1@gmail.com>
2022-07-05 22:57:46 +0900

    モデルとデータを準備

---
+++ Shared/Fitness/Model/Ring.swift
@@ -0,0 +1,25 @@
+//
+//  Ring.swift
+//  src (iOS)
+//
+//  Created by Sho Emoto on 2022/07/05.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: Progress Ring Model and Sample Data
+struct Ring {
+    let id = UUID().uuidString
+    let progress: CGFloat
+    let value: String
+    let keyIcon: String
+    let keyColor: Color
+    let isText: Bool
+}
+
+let rings = [
+    Ring(progress: 72, value: "Steps", keyIcon: "figure.walk", keyColor: Color("Crimson"), isText: false),
+    Ring(progress: 36, value: "Calories", keyIcon: "flame.fill", keyColor: Color("LawnGreen"), isText: false),
+    Ring(progress: 91, value: "Sleep Time", keyIcon: "figure.walk", keyColor: Color("Aqua"), isText: true)
+]
