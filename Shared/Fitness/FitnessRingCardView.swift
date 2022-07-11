7eae483d0cbe0f1312166670b5a8d2f1fcec025e
Sho Emoto <dd2d00seg1@gmail.com>
2022-07-11 21:56:14 +0900

    RingViewのカスタマイズ

--- Shared/Fitness/FitnessRingCardView.swift
+++ Shared/Fitness/FitnessRingCardView.swift
@@ -65,6 +65,62 @@ struct FitnessRingCardView: View {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
                 .fill(Color("MidnightBlue"))
         )
+
+        VStack(spacing: 15) {
+
+            Text("Progress")
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            HStack(spacing: 10) {
+
+                ZStack {
+
+                    ForEach(rings.indices, id: \.self) { index in
+
+                        AnimatedMultiRingView(ring: rings[index], index: index)
+                    }
+
+                }
+                .frame(width: 130, height: 130)
+
+                VStack(alignment: .leading, spacing: 12) {
+
+                    ForEach(rings) { ring in
+
+                        Label {
+
+                            HStack(alignment: .bottom, spacing: 6) {
+
+                                Text("\(Int(ring.progress))%")
+                                    .font(.title3.bold())
+
+                                Text(ring.value)
+                                    .font(.caption)
+                            }
+                        } icon: {
+
+                            Group {
+
+                                Image(systemName: ring.keyIcon)
+                                    .font(.title2)
+                                    .foregroundColor(ring.keyColor)
+                            }
+                            .frame(width: 30)
+                        }
+                    }
+                }
+                .padding(.leading, 10)
+            }
+            .padding(.top, 20)
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 25)
+        .background(
+
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(Color("MidnightBlue"))
+        )
     }
 }
 
@@ -91,6 +147,57 @@ struct AnimatedRingView: View {
                 .trim(from: 0, to: showRing ? rings[index].progress / 100 : 0)
                 .stroke(
                     rings[index].keyColor,
+                    style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
+                )
+            // NOTE: 角度調整
+                .rotationEffect(.init(degrees: -90))
+        }
+        .padding(CGFloat(index) * 16)
+        .onAppear {
+
+            // NOTE: Show After Initial Animation Finished
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+
+                withAnimation(
+                    .interactiveSpring(
+                        response: 1,
+                        dampingFraction: 1,
+                        blendDuration: 1
+                    ).delay(Double(index) * 0.1)
+                ) {
+                    showRing = true
+                }
+            }
+        }
+    }
+}
+
+struct AnimatedMultiRingView: View {
+    var ring: Ring
+    var index: Int
+    @State var showRing = false
+
+    var body: some View {
+
+        ZStack {
+            Circle()
+                .stroke(.gray.opacity(0.3), lineWidth: 10)
+
+            Circle()
+            // NOTE: 塗り潰しの設定
+                .trim(from: 0, to: showRing ? rings[index].progress / 100 : 0)
+                .stroke(
+                    rings[index].keyColor,
+                    style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
+                )
+            // NOTE: 角度調整
+                .rotationEffect(.init(degrees: -90))
+
+            Circle()
+            // NOTE: 塗り潰しの設定
+                .trim(from: rings[index].progress / 100, to: showRing ? 1:rings[index].progress / 100)
+                .stroke(
+                    .white,
                     style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
                 )
             // NOTE: 角度調整
--- Shared/Fitness/FitnessScreen.swift
+++ Shared/Fitness/FitnessScreen.swift
@@ -9,29 +9,15 @@ import SwiftUI
 
 struct FitnessScreen: View {
 
-    // MARK: Animation Properties
-    @State var showView = false
-
     var body: some View {
         VStack {
 
             FitnessRingCardView()
-                .opacity(showView ? 1 : 0)
-                .offset(y: showView ? 0 : 250)
         }
         .foregroundColor(.white)
         .padding()
-        .onAppear(perform: animateView)
     }
 
-    // MARK: Animating View
-    func animateView() {
-
-        withAnimation(.easeInOut.delay(0.5)) {
-
-            showView = true
-        }
-    }
 }
 
 struct FitnessScreen_Previews: PreviewProvider {
