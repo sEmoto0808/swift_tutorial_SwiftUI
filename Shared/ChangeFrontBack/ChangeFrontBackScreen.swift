03bc057f4d8a1751e6c5ef902336cddb19d0eca8
Sho Emoto <dd2d00seg1@gmail.com>
2022-10-16 18:22:42 +0900

    ZStackで要素を配置

--- Shared/ChangeFrontBack/ChangeFrontBackScreen.swift
+++ Shared/ChangeFrontBack/ChangeFrontBackScreen.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct ChangeFrontBackScreen: View {
     var body: some View {
-        Text("ChangeFrontBack")
+        VStack(spacing: 0.0) {
+            ZStack {
+                itemViewBlue
+                    .offset(x: -20, y: 11)
+
+                itemViewRed
+                    .offset(x: 20, y: -11)
+            }
+
+            Button {} label: {
+                Text("Tap to Change")
+                    .foregroundColor(.white)
+                    .background(
+                        Color.black
+                        .contentShape(Rectangle())
+                        .frame(width: 150, height: 44)
+                    )
+            }
+        }
+    }
+
+    var itemViewBlue: some View {
+        Text("")
+            .background(
+                Color.blue
+                .contentShape(Rectangle())
+                .frame(width: 170, height: 300)
+            )
+    }
+
+    var itemViewRed: some View {
+        Text("")
+            .background(
+                Color.red
+                .contentShape(Rectangle())
+                .frame(width: 170, height: 300)
+            )
     }
 }
 
