0a83e3c262ba0c1f61d2b41caa74a9c7f5b85d97
Sho Emoto <dd2d00seg1@gmail.com>
2023-02-12 20:43:48 +0900

    CMMotionManagerのセットアップ

--- Shared/Animation/HolographicEffect/HolographicEffectScreen.swift
+++ Shared/Animation/HolographicEffect/HolographicEffectScreen.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HolographicEffectScreen: View {
+
+    @ObservedObject private var manager = MotionManager()
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
---
+++ Shared/Animation/HolographicEffect/MotionManager.swift
@@ -0,0 +1,27 @@
+//
+//  MotionManager.swift
+//  src (iOS)
+//
+//  Created by Sho Emoto on 2023/02/12.
+//
+
+import SwiftUI
+import CoreMotion
+
+/// 端末のMotion Dataを取得するクラス
+///
+/// - Accelerometer data, indicating the instantaneous acceleration of the device in three dimensional space.
+/// - Gyroscope data, indicating the instantaneous rotation around the device's three primary axes.
+/// - Magnetometer data, indicating the device's orientation relative to Earth's magnetic field.
+/// - Device-motion data, indicating key motion-related attributes such as the device's user-initiated acceleration, its attitude, rotation rates, orientation relative to calibrated magnetic fields, and orientation relative to gravity. This data is provided by Core Motion’s sensor fusion algorithms.
+final class MotionManager: ObservableObject {
+
+    private let manager: CMMotionManager
+
+    init() {
+        manager = CMMotionManager()
+        manager.accelerometerUpdateInterval = 1/60
+        manager.gyroUpdateInterval = 1/60
+        manager.deviceMotionUpdateInterval = 1/60
+    }
+}
